f3b7b5d46fcdf68fb139a5dbb94bc719c74efa4a
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-02-27 02:00:37 +0900

    Setting the locale from the subdomain name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  # Rails Internationalization (I18n) API - Ruby on Rails Guides.
+  # http://guides.rubyonrails.org/i18n.html#setting-the-locale-from-the-domain-name)
+  def set_locale
+    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+  end
+
+  # Get locale code from request subdomain (like http://jp.application.dev)
+  # You have to create the Pow symlink like:
+  #   ln -s plate ~/.pow/plate
+  def extract_locale_from_subdomain
+    locale = request.subdomains.first
+    I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
+  end
 end
