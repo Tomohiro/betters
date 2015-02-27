1a3baedb515aeb357e558fe8942805cf78c86bc3
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-02-27 09:21:50 +0900

    Update method comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,20 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  # Set I18n locale
+  #
   # Rails Internationalization (I18n) API - Ruby on Rails Guides.
-  # http://guides.rubyonrails.org/i18n.html#setting-the-locale-from-the-domain-name)
+  # http://guides.rubyonrails.org/i18n.html#setting-the-locale-from-the-domain-name
   def set_locale
     I18n.locale = extract_locale_from_subdomain || I18n.default_locale
   end
 
-  # Get locale code from request subdomain (like http://jp.application.dev)
-  # You have to create the Pow symlink like:
+  # Get locale code from request subdomain (like http://ja.plate.dev)
+  #
+  # In development environment on OS X, you have to create the Pow symlink like:
   #   ln -s plate ~/.pow/plate
+  #
+  # @return [String|nil] locale the auto detected locale from subdomain
   def extract_locale_from_subdomain
     locale = request.subdomains.first
     I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
