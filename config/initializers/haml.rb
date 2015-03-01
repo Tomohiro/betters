5e92a2a6e26d17aea9e50c5a6a5ca63b07bac523
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-03-01 13:36:57 +0900

    Set Haml options

---
+++ config/initializers/haml.rb
@@ -0,0 +1,5 @@
+# File: REFERENCE — Haml Documentation
+# http://haml.info/docs/yardoc/file.REFERENCE.html
+
+# Always use double quotes, never single quotes, on attributes.
+Haml::Template.options[:attr_wrapper] = '"'
